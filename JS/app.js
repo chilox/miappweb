55c30f08c79e5693c1f532ce0835b6471e2aa993
Ilich Stanyn Quiñonez Seijas <tuher2u@gmail.com>
2022-05-18 19:47:39 -0300

    Se agrega validaciones al login

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,22 @@
+function login_user_ok(){
+    let admin = 'tuherou@gmail.com'
+    let passAdmin = 123456789
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('password').value;   
+    if(email === admin && pass == passAdmin){
+        window.location.href = 'home.html'
+    }else{        
+        alert('El usuario o contraseña no existe')
+    }
+}
+
 function login_validate(){
     let email = document.getElementById('email').value;
-    let pass = document.getElementById('password').value;
+    let pass = document.getElementById('password').value;    
     if (email == ''){
         alert('Ingresa un mail correcto')
     }else if(pass == '' || pass === ' ' || pass.length <= 6){
         alert('Ingresa una contraseña')
-    } else {
-    window.location.href = 'home.html'
-    
     }
 }
 
@@ -43,4 +52,28 @@ function validarUrl(){
     c.style.backgroundColor = 'blue';
 }
 
+}
+
+function registro_validate(){
+
+    let nombre = document.querySelector(".cont-registro__nombre").value;
+    let apellido = document.querySelector(".cont-registro__apellido").value;
+    let email = document.querySelector(".cont-registro__email").value;
+    let numero = document.querySelector(".cont-registro__number").value;
+    let password = document.querySelector(".cont-registro__pass").value;
+    if(nombre == '' || nombre === ' '){
+        alert('Ingrese el nombre')
+    } else if (apellido == '' || apellido === ' '){
+        alert('Ingrese el Apellido')
+    } else if (email == ''){
+        alert('Ingresa un mail correcto')
+    }else if(numero == '' || numero === ' ' || numero.length <= 9){
+        alert('Ingrese un numero telefonico valido')
+    }else if(password == '' ){
+        alert('Ingresa una contraseña')
+    } else {
+        alert('El registro se realizo correctamente')
+        console.log(nombre + ' ' + apellido) 
+    }
+           
 }
